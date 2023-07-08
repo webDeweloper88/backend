a8b793c483608a66582dacc32ecf7fe13ad7af7c
Makhmud Khudoyberdiev <m.xudoyberdiev88@gmail.com>
2023-07-08 22:42:10 +0500

    fix docker-compose

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [], // Импортируйте здесь необходимые модули
+  controllers: [AppController], // Добавьте ваш контроллер здесь
+  providers: [AppService], // Добавьте ваши провайдеры (сервисы) здесь
 })
 export class AppModule {}
